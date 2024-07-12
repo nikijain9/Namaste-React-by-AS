05ba49fb284898d804f939279d304d9815f9ef3f
nikijain9 <nikitajain267@gmail.com>
2024-07-12 20:53:31 +0530

    Rendering by calling API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,41 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData"
+import { useEffect, useState } from "react";
 
 const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6010921&lng=73.7641245&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+
+        const json = await data.json();
+        console.log(json);
+        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
     return (
         <div className="body">
             <div className="filter">
-            Search
+            <button
+            onClick={() => {
+                    const filteredList = listOfRestaurants.filter(
+                        (res) => res.info.avgRating > 4.5
+                    );
+                    setListOfRestaurants(filteredList);
+                }}
+            >
+            Top Rated Restaurants
+            </button>
             </div>
 
             <div className="resContainer">
                 {
-                    resList.map((restaurant) => {
+                    listOfRestaurants.map((restaurant) => {
                         return <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     })
                 }
